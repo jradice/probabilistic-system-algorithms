ce11388905fa9ed7e83c26c9819d580d990fbdfa
Prasad, Ashita Rajesh <aprasad47@gatech.edu>
2016-09-20 18:02:40 -0400

    Added files via upload

---
+++ probability_solution.py
@@ -0,0 +1,15 @@
+"""Testing pbnt. Run this before anything else to get pbnt to work!"""
+import sys
+if('pbnt/combined' not in sys.path):
+    sys.path.append('pbnt/combined')
+from exampleinference import inferenceExample
+
+inferenceExample()
+# Should output:
+# ('The marginal probability of sprinkler=false:', 0.80102921)
+#('The marginal probability of wetgrass=false | cloudy=False, rain=True:', 0.055)
+
+'''
+WRITE YOUR CODE BELOW. DO NOT CHANGE ANY FUNCTION HEADERS FROM THE NOTEBOOK.
+'''
+
